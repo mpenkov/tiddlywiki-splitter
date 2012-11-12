a81fcce1f3e2a7434f102b76272b04ab87a719f1
Michael Penkov <misha.penkov@gmail.com>
2012-11-12 15:53:16 +0900

    Added code to handle TiddlyWiki tags.

--- formatter.py
+++ formatter.py
@@ -27,6 +27,32 @@ if __name__ == "__main__":
         line = re.sub("//", "_", line)
         lines[i] = line
 
+    #
+    # Handle tags.
+    #
+    try:
+        raw_txt = filter(lambda l: l.startswith("tags:"), lines)[0][6:]
+        raw_txt = raw_txt.replace("[[", " [[ ").replace("]]", " ]] ")
+        tokens = filter(None, raw_txt.split(" "))
+        tags = list()
+        escape = False
+        start = 0
+        for i,t in enumerate(tokens):
+            if t == "[[":
+                escape = True
+                start = i+1
+            elif t == "]]":
+                escape = False
+                tags.append("_".join(tokens[start:i]))
+            elif not escape:
+                tags.append(t)
+
+        for i,l in enumerate(lines):
+            if l.startswith("tags:"):
+                lines[i] = " ".join(map(lambda t: "@%s" % t, tags))
+    except IndexError:
+        pass
+
     import codecs
     fout = codecs.open(sys.argv[1], "w", "utf-8")
     fout.write("\n".join(lines))
